d1e1fa9bd0bb40fe273ab0d509c50efe773fa4d0
fconsoni <franco.consoni@gmail.com>
2019-10-11 11:40:27 -0300

    Add files via upload

---
+++ XibView.swift
@@ -0,0 +1,86 @@
+//
+//  XibView.swift
+//  Prebo
+//
+//  Created by Franco Consoni on 05/07/2019.
+//  Copyright © 2019 Kickser S.A. All rights reserved.
+//
+
+import UIKit
+
+class XibControl: UIControl {
+    var xibName: String?
+    
+    required override init(frame: CGRect = CGRect.zero) {
+        super.init(frame: frame); commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder); commonInit()
+    }
+    
+    internal func commonInit() {
+        setupFromXib()
+    }
+    
+    private func setupFromXib() {
+        let xibName = self.xibName ?? String(describing: type(of: self))
+        
+        let viewsInXib = Bundle(for: type(of: self)).loadNibNamed(xibName, owner: self, options: nil)
+        guard let view = viewsInXib?.first as? UIView else {
+            return
+        }
+        
+        self.addSubview(view)
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: self.topAnchor),
+            view.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            view.rightAnchor.constraint(equalTo: self.rightAnchor),
+            view.leftAnchor.constraint(equalTo: self.leftAnchor),
+            ])
+        
+        self.backgroundColor = UIColor.clear
+    }
+    
+}
+
+class XibView: UIView {
+    var xibName: String?
+    
+    required override init(frame: CGRect = CGRect.zero) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    internal func commonInit() {
+        setupFromXib()
+    }
+    
+    private func setupFromXib() {
+        let xibName = self.xibName ?? String(describing: type(of: self))
+        
+        let viewsInXib = Bundle(for: type(of: self)).loadNibNamed(xibName, owner: self, options: nil)
+        guard let view = viewsInXib?.first as? UIView else {
+            return
+        }
+        
+        self.addSubview(view)
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: self.topAnchor),
+            view.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            view.rightAnchor.constraint(equalTo: self.rightAnchor),
+            view.leftAnchor.constraint(equalTo: self.leftAnchor),
+            ])
+        
+        self.backgroundColor = UIColor.clear
+    }
+}
